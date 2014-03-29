3153b2e387641dcd2184728b2b438055463084e8
Jim Kang <jimkang@gmail.com>
2014-03-29 14:19:00 -0400

    Changed shuffle to in-place shuffle.

--- chrome/@shuffle.js
+++ chrome/@shuffle.js
@@ -13,7 +13,6 @@
 
   // http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modern_method
   function shuffle(array) {
-    var shuffled = [];
     var swapIndex1 = null;
     var swapIndex1UpperLimit = array.length;
     var swapIndex2 = array.length - 1;
@@ -22,21 +21,15 @@
     for (var i = 0; i < array.length; ++i) {
       swapIndex1 = ~~(Math.random() * swapIndex1UpperLimit);
 
-      if (shuffled[swapIndex1] === undefined) {
-        swapee =  array[swapIndex1];
-      }
-      else {
-        swapee = shuffled[swapIndex1];
-      }
-
-      shuffled[swapIndex1] = array[swapIndex2];
-      shuffled[swapIndex2] = swapee;
+      swapee =  array[swapIndex1];
+      array[swapIndex1] = array[swapIndex2];
+      array[swapIndex2] = swapee;
 
       swapIndex1UpperLimit -= 1;
       swapIndex2 -= 1;
     }
 
-    return shuffled;
+    return array;
   }
 
   var tweetTexts = document.querySelectorAll('.tweet-text');
@@ -49,6 +42,6 @@
   tweetTexts.forEach(function replaceWithShuffled(tweetText, i) {
     tweetText.innerHTML = shuffledContents[i];
   });
-  
+
 })());
 
